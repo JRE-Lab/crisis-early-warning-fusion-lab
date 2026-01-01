7a58dbf4b520467de64db3c9c3dec49481f21622
JRE <162952592+JRE-Lab@users.noreply.github.com>
2025-12-31 23:12:05 -0600

    Add __main__.py to execute CLI main function

---
+++ fusion_lab/__main__.py
@@ -0,0 +1,11 @@
+diff --git a/fusion_lab/__main__.py b/fusion_lab/__main__.py
+new file mode 100644
+index 0000000000000000000000000000000000000000..1c8b4f8ee4ee048fd2e96c21b9912ead576e1f54
+--- /dev/null
++++ b/fusion_lab/__main__.py
+@@ -0,0 +1,5 @@
++from fusion_lab.cli import main
++
++
++if __name__ == "__main__":
++    main()
